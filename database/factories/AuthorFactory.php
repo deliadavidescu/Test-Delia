a0481eb3efbb1caf4c85be102fa92e7e4b79468a
Delia Davidescu <deliaalina.davidescu@gmail.com>
2019-08-20 23:20:05 +0200

    create factory for author table

---
+++ database/factories/AuthorFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Author;
+use Faker\Generator as Faker;
+
+$factory->define(Author::class, function (Faker $faker) {
+    return [
+      'name' =>  $faker->name,
+        'age' =>  $faker->numberBetween(19,100),
+        'address' =>  $faker->address
+    ];
+});
