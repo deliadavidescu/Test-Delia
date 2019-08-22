7b9e858f30e759119616ebd34364cb633928a936
Delia Davidescu <deliaalina.davidescu@gmail.com>
2019-08-22 10:51:11 +0200

    fix create  route error

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('books','BooksController@index');
-Route::get('create','BooksController@store');
+Route::post('create','BooksController@store');
